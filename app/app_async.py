dd02609eb8a5a8c305d1fec635171cebe550dcb5
Diogo Dutra <diogodutra@gmail.com>
2019-10-27 01:03:22 -0400

    fix

--- app/app_async.py
+++ app/app_async.py
@@ -69,9 +69,8 @@ async def scrap(request):
     scrapper = Scrapper()
     scrapper.request(url)
     async with scrapper.extract_text() as scrapped_text:
-        json = {'url': url, 'text': await scrapped_text}
-        
-    return web.json_response(json)
+        json = {'url': url, 'text': await scrapped_text}        
+        return await web.json_response(json)
     
 
 @routes.get("/googler/{keywords}")
