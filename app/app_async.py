65c58e240e1a4fd317f1d22000ca65cc53e19cc8
Diogo Dutra <diogodutra@gmail.com>
2019-10-27 17:22:57 -0400

    fix

--- app/app_async.py
+++ app/app_async.py
@@ -70,9 +70,9 @@ async def scrap(request):
     scrapper = Scrapper()
     # scrapper.request(url)
     scrapper.make_request(url)
-    print(await scrapper.text)
-    json = {'url': url, 'text': await scrapper.text}    
-    return await web.json_response(await json)
+    # print(await scrapper.text)
+    json = {'url': url, 'text': scrapper.text}    
+    return web.json_response(json)
     # loop = asyncio.get_event_loop()
     # loop.run_until_complete(scrapper.make_request(url))
     # async with scrapper.extract_text() as scrapped_text:
