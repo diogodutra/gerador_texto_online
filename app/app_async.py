c0680226e453708ec4a880c88aaef273a8d93aa1
Diogo Dutra <diogodutra@gmail.com>
2019-10-27 20:49:35 -0400

    fix googler

--- app/app_async.py
+++ app/app_async.py
@@ -69,16 +69,13 @@ async def web_search(request):
     keywords = request.match_info['keywords']
     country = 'br'
     googler = Googler()
-    # googler.google(keywords, country=country)
     googler.google(keywords, country=country)
     json = {}
     for i in range(len(googler.urls)):
-        # json[i] = {'url': urls[j], 'title': titles[j], 'description': descriptions[j]}
-        j = i
         json[i] = {
-                'url': googler.urls[j],
-                'title': googler.titles[j],
-                'description': googler.descriptions[j]
+                'url': googler.urls[i],
+                'title': googler.titles[i],
+                'description': googler.descriptions[i]
                 }
 
     return web.json_response(json)
