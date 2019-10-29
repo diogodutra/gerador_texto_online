8ed3a8c316b29d646cc53d5eccb0eb9e7d6e0b68
Diogo Dutra <diogodutra@gmail.com>
2019-10-29 19:20:34 -0400

    fix blogger

--- app/app_async.py
+++ app/app_async.py
@@ -102,7 +102,9 @@ async def blogger(request):
     googler.google(keywords, country=country)
     
     tasks = [scrapper_spinner(url) for url in googler.urls]
-    await asyncio.gather(*tasks)
+    # await asyncio.gather(*tasks)
+    loop = asyncio.get_event_loop()
+    texts = loop.run_until_complete(asyncio.gather(*tasks))
 
     json = {}
     # for i in range(len(googler.urls)):
@@ -110,7 +112,7 @@ async def blogger(request):
         json[i] = {
                 'url': googler.urls[i],
                 'title': googler.titles[i],
-                'text': tasks[i].result()
+                'text': texts[i]
                 }
 
     return web.json_response(json)
