dc942237464fe38864e883feb48dc6e24edc132d
Diogo Dutra <diogodutra@gmail.com>
2019-10-29 21:15:34 -0400

    fix html

--- client/app1.js
+++ client/app1.js
@@ -279,10 +279,12 @@ var App = function(){
         //     window.alert(document.getElementById(this.id_keywords).textContent)
         //     this.compose()
         //   });
-        document.getElementById(this.id_button).onclick = function(){
-            window.alert(document.getElementById(this.id_keywords).textContent)
-            this.compose()
-        };
+        // document.getElementById(this.id_button).onclick = function(){
+        //     window.alert(document.getElementById(this.id_keywords).textContent)
+            //         console.log("Geo location reset clicked")
+        //     this.compose()
+        // };
+        document.getElementById(this.id_button).onclick = this.compose()
     }
 
     this.compose = function(){
