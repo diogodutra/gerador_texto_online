8d94223512ef0f95480be01432aa9c5e075137b6
Diogo Dutra <diogodutra@gmail.com>
2019-11-02 21:08:41 -0400

    new html

--- client/app1.js
+++ client/app1.js
@@ -33,46 +33,7 @@ var App = function(){
         var div_base = document.getElementById(this.id_results)
         for (var idx in Texts) {
             // alert("for: ".concat(showProps(Texts[idx], "Texts")))
-
             createText(Texts[idx], idx)
-            
-            // var t = Texts[idx]
-            
-            // // create DOM for each result
-            // var new_div = document.createElement("div")
-            // var new_title = document.createElement("h1")
-            // var new_text = document.createElement("p")
-            // var new_icon_container = document.createElement("div")
-            // var new_icon = document.createElement("div")
-            // var new_icon_image = document.createElement("img")
-
-            // // fill new DOM with content
-            // new_title.innerHTML = t.title
-            // new_text.innerHTML = t.text
-            
-            // //
-            // new_icon_image.addEventListener("click", function(){
-            //     alert("click: ")
-            // });
-            
-            // new_div.setAttribute("id", "div_".concat(idx))
-            // new_div.setAttribute("id", "title_".concat(idx))
-            // new_div.setAttribute("id", "text_".concat(idx))
-
-            // new_div.setAttribute("class", "grid-item")
-            // new_title.setAttribute("class", "title")
-            // new_text.setAttribute("class", "text")
-            // new_icon_container.setAttribute("class", "icon-container")
-            // new_icon.setAttribute("class", "icon")
-            // new_icon_image.setAttribute("class", "image-icon")
-            // new_icon_image.src = "images/copy.png"
-
-            // new_icon.appendChild(new_icon_image)
-            // new_icon_container.appendChild(new_icon)
-            // new_div.appendChild(new_icon_container)
-            // new_div.appendChild(new_title)
-            // new_div.appendChild(new_text)
-            // div_base.appendChild(new_div)
         }
     }
 
@@ -84,17 +45,15 @@ var App = function(){
         var language = document.getElementById(this.id_language).value
         keywords = encodeURI(keywords)
         if (keywords) {
-            alert("get started.")
             app.getTexts(keywords, country, language).then((Texts) => {
                 if (Texts) {
-                    alert("get finished with success.")
                     app.updateTexts(Texts)
                 } else {
-                    alert("get finished with empty result.")
+                    // alert("get finished with empty result.")
                 }
             }, reason => {
               console.error(reason); // Error!
-              alert("get finished with error: ".concat(reason))
+            //   alert("get finished with error: ".concat(reason))
             })
         }
     }
@@ -214,16 +173,16 @@ document.addEventListener("DOMContentLoaded", (evt)=>{
     app.init()
 })
 
-var Texts = []
-Texts[0] = []
-Texts[1] = []
-Texts[2] = []
-Texts[0].title = "Title 0"
-Texts[1].title = "Title 1"
-Texts[2].title = "Title 2"
-Texts[0].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
-Texts[1].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
-Texts[2].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
+// var Texts = []
+// Texts[0] = []
+// Texts[1] = []
+// Texts[2] = []
+// Texts[0].title = "Title 0"
+// Texts[1].title = "Title 1"
+// Texts[2].title = "Title 2"
+// Texts[0].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
+// Texts[1].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
+// Texts[2].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
 
-createAllTexts(Texts)
+// createAllTexts(Texts)
 // alert("Texts.length: ".concat(Texts.length))
