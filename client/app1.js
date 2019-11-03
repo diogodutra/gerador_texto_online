cb37109837e1c1238826ae6352db804f99b52e29
Diogo Dutra <diogodutra@gmail.com>
2019-11-02 21:33:37 -0400

    new html

--- client/app1.js
+++ client/app1.js
@@ -9,6 +9,7 @@ var App = function(){
     this.id_results = "results"
     this.id_country = "country"
     this.id_language = "language"
+    this.id_loader = "loader"
 
     this.getTexts = function(keywords, country, language) {
     // inspiration: this.getPlanetEphemeris = function(planetName){
@@ -45,8 +46,10 @@ var App = function(){
         var language = document.getElementById(this.id_language).value
         keywords = encodeURI(keywords)
         if (keywords) {
+            document.getElementById("loader").style.display = "block"
             app.getTexts(keywords, country, language).then((Texts) => {
                 if (Texts) {
+                    document.getElementById("loader").style.display = "none"
                     app.updateTexts(Texts)
                 } else {
                     // alert("get finished with empty result.")
@@ -55,6 +58,8 @@ var App = function(){
               console.error(reason); // Error!
             //   alert("get finished with error: ".concat(reason))
             })
+        } else {
+            document.getElementById("loader").style.display = "none"
         }
     }
 
@@ -177,7 +182,7 @@ document.addEventListener("DOMContentLoaded", (evt)=>{
 // Texts[0] = []
 // Texts[1] = []
 // Texts[2] = []
-// Texts[0].title = "Title 0"
+// Texts[0].title = "Title 0 muito grande para caber numa só linha"
 // Texts[1].title = "Title 1"
 // Texts[2].title = "Title 2"
 // Texts[0].text = "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him."
